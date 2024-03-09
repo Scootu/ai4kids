1b6f04ac6354fac82a07944f5cb885ea353c1be9
aneshamdaoui <bacetd2004@gmail.com>
2024-03-09 19:38:22 +0100

    add :action deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const route = createBrowserRouter([{ path: "/ai4kids", element: <App /> }]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={route} />
+  </React.StrictMode>
+);
